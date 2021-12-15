54747f44a0cacf8a34f3ddaaf67764ea36abee58
Ankurhash <ankur56@protonmail.com>
2021-12-15 11:32:31 +0000

    Initial Commit

---
+++ helper.py
@@ -0,0 +1,28 @@
+import subprocess
+import datetime
+import asyncio
+import os
+import requests
+
+import aiohttp
+import tgcrypto
+import aiofiles
+
+def duration(filename):
+    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
+                             "format=duration", "-of",
+                             "default=noprint_wrappers=1:nokey=1", filename],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+    return float(result.stdout)
+
+
+async def download(url):
+    ka = f'{url.split("/")[-1]}.pdf'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(ka, mode='wb')
+                await f.write(await resp.read())
+                await f.close()
+    return ka
